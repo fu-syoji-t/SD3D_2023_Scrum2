a5e3d3809c378838bf273da9e6e4ca22d3875268
福山拓功 <117243226+FHiraku@users.noreply.github.com>
2023-11-17 09:26:04 +0900

    Update AddedApointment.php

--- 04インクリメント/AddedApointment.php
+++ 04インクリメント/AddedApointment.php
@@ -1,24 +1,25 @@
 <link rel="stylesheet" href="css/thread.css">
 <form name="loguinkanryou_form">
-<div class=back>  
-<div style="text-align:left;"> 
-    <?php
-            date_default_timezone_set("japan");
-            echo date("Y/m/d");
-        ?>
-        <br>
+<div class=back>
         </div>
-        <div style="text-align:center;">
-        <div id="headerdiv">
-            <img src="img/logo.png">
-        </div>
-    </div>
 </div>
 <div style="text-align:center;">
     <h1>予定追加しました</h1>
     <div class="section1 text-center">
-    <button type="button" class="btn btn-outline-dark" type="button" onclick="location.href='calendar.php'">　　次へ　　</button>
+    <?php
+  //input.phpの値を取得 
+    $pdo = new PDO('mysql:host=localhost;dbname=daysshare;charset=utf8','root','');//PDOでMySQLのデータベースに接続
+    $sql = "INSERT INTO schedule(startday,importance,title,memo) VALUES (?,?,?,?)";
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(1,$_POST['startday'],PDO::PARAM_STR);
+    $ps->bindValue(2,$_POST['importance'],PDO::PARAM_INT);
+    $ps->bindValue(3,$_POST['title'], PDO::PARAM_STR);
+    $ps->bindValue(4,$_POST['memo'],PDO::PARAM_STR);
+    $ps->execute();  
+    $searchArray = $ps->fetchAll();
+?>
   </div>
+  <button type="button" class="btn btn-outline-dark" type="button" onclick="location.href='../calendar/calendar.php'">次へ</button>
 </div>
 </div>   
 </form>
