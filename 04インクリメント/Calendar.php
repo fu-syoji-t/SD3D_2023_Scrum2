5944f823258e02bdb5c85a8d79565b4c58c27e10
福山拓功 <117243226+FHiraku@users.noreply.github.com>
2023-11-29 11:26:51 +0900

    Update Calendar.php

--- 04インクリメント/Calendar.php
+++ 04インクリメント/Calendar.php
@@ -1,121 +1,169 @@
 <?php
-$today = filter_input(INPUT_POST, 'today');
-$monthNext = filter_input(INPUT_POST, 'monthNext');
-$yearNext = filter_input(INPUT_POST, 'yearNext');
-$monthPrev = filter_input(INPUT_POST, 'monthPrev');
-$yearPrev = filter_input(INPUT_POST, 'yearPrev');
 
+session_start();
+require '../DAO.php';
+$dao = new DAO();
 
-if($today==1){
-    $month = date('n');
-    $year = date('Y');
+// タイムゾーンを設定
+date_default_timezone_set('Asia/Tokyo');
+
+// 前月・次月リンクが押された場合は、GETパラメーターから年月を取得
+if (isset($_GET['ym'])) {
+    $ym = $_GET['ym'];
+} else {
+    // 今月の年月を表示
+    $ym = date('Y-m');
 }
-if($monthNext > 12){
-    $monthNext = 1;
-    $yearNext++;
+
+// タイムスタンプを作成し、フォーマットをチェックする
+$timestamp = strtotime($ym . '-01');
+if ($timestamp === false) {
+    $ym = date('Y-m');
+    $timestamp = strtotime($ym . '-01');
 }
-if($monthPrev === "0"){
-    $monthPrev = 12;
-    $yearPrev--;
-}
-$month = $monthNext??$monthPrev??date('n');
-$year =$yearNext??$yearPrev??date('Y');
-
-$last_day = date('j', mktime(0, 0, 0, $month + 1, 0, $year));
-$calendar = array();
-$j = 0;
 
 
+// 今日の日付 フォーマット　例）2021-06-3
+$today = date('Y-m-j');
 
-for ($i = 1; $i < $last_day + 1; $i++) {
-    $week = date('w', mktime(0, 0, 0, $month, $i, $year));
-    if ($i == 1) {
-        for ($s = 1; $s <= $week; $s++) {
-            $calendar[$j]['day'] = '';
-            $j++;
-        }
+// カレンダーのタイトルを作成　例）2021年6月
+$html_title = date('Y年n月', $timestamp);
+
+
+// 方法１：mktimeを使う mktime(hour,minute,second,month,day,year)
+$prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
+$next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
+
+// 方法２：strtotimeを使う
+// $prev = date('Y-m', strtotime('-1 month', $timestamp));
+// $next = date('Y-m', strtotime('+1 month', $timestamp));
+
+// 該当月の日数を取得
+$day_count = date('t', $timestamp);
+
+// １日が何曜日か　0:日 1:月 2:火 ... 6:土
+// 方法１：mktimeを使う
+$youbi = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
+// 方法２
+// $youbi = date('w', $timestamp);
+
+
+// カレンダー作成の準備
+$weeks = [];
+$week = '';
+
+
+
+// 第１週目：空のセルを追加
+// 例）１日が火曜日だった場合、日・月曜日の２つ分の空セルを追加する
+$week .= str_repeat('<td></td>', $youbi);
+
+
+$schedule = $dao->schedule_hyouji($_SESSION['group_id']);
+ 
+echo "<br><br>";
+for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
+    // 2021-06-3
+    $date = $ym . '-' . $day;
+
+    if ($today == $date) {
+        // 今日の日付の場合は、class="today"をつける
+        $week .= '<td class="today">' . $day;
+    } else {
+        $week .= '<td>' . $day;
     }
-    $calendar[$j]['day'] = $i;
-    $j++;
-    if ($i == $last_day) {
-        for ($e = 1; $e <= 6 - $week; $e++) {
-            $calendar[$j]['day'] = '';
-            $j++;
+    foreach($schedule as $row){
+
+        $date2 = strtotime($date);
+        $startday = strtotime($row["startday"]);
+        $endday = strtotime($row["endday"]);
+        $schedule_class = "";
+
+        if ($startday <= $date2 && $date2 <= $endday) {
+            $schedule_class = strlen($row["title"]) > 5 ? 'long-text' : '';
+            $week .= '<br><button type="submit" formaction="../apointment/Apointment_check.php" name="schedule" class="yotei_button'.$schedule_class.'" value='.$row["schedule_id"].'>'.$row["title"].'</button>';
         }
+
+    }
+    
+    $week .= '</td>';
+
+    // 週終わり、または、月終わりの場合
+    if ($youbi % 7 == 6 || $day == $day_count) {
+
+        if ($day == $day_count) {
+            // 月の最終日の場合、空セルを追加
+            // 例）最終日が水曜日の場合、木・金・土曜日の空セルを追加
+            $week .= str_repeat('<td></td>', 6 - $youbi % 7);
+        }
+
+        // weeks配列にtrと$weekを追加する
+        $weeks[] = '<tr>' . $week . '</tr>';
+
+        // weekをリセット
+        $week = '';
     }
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <title>カレンダー</title>
-  <link href="DaysShare/css/style.css" rel="stylesheet" type="text/css">
-</head>
+    <meta charset="utf-8">
+    <title>PHPカレンダー</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css"> 
 
+<div class="header">
+    </div>
+</head>
 <body>
-  <table>
-    <thead>
-      <tr>
-        <form action="" method="post">
-          <th><button type="submit" id="prev">
-              &laquo;
-              <input type="hidden" name="monthPrev" value="<?php echo $month-1;?>">
-              <input type="hidden" name="yearPrev" value="<?php echo $year;?>">
-            </button></th>
-        </form>
-        <th id="title" colspan="5"><?php echo $year; ?>年<?php echo $month; ?>月
-        </th>
-        <form action="" method="post">
-          <th><button type="submit" id="next">
-              &raquo;
-              <input type="hidden" name="monthNext" value="<?php echo $month+1;?>">
-              <input type="hidden" name="yearNext" value="<?php echo $year;?>">
-            </button></th>
-        </form>
-      </tr>
-      <tr>
-        <th class="red">日</th>
-        <th>月</th>
-        <th>火</th>
-        <th>水</th>
-        <th>木</th>
-        <th>金</th>
-        <th class="blue">土</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <?php $cnt = 0; ?>
-        <?php foreach ($calendar as $key => $value): ?>
-        <td>
-          <p>
-            <?php $cnt++; ?>
-            <?php echo $value['day']; ?>
-          </p>
-        </td>
-        <?php if ($cnt == 7): ?>
-      </tr>
-      <tr>
-        <?php $cnt = 0; ?>
-        <?php endif; ?>
-        <?php endforeach; ?>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <form action="" method="post">
-          <td colspan="7">
-            <button type="submit" id="today"> 現在に戻る
-              <input type="hidden" name="today" value="1">
-            </button>
-          </td>
-        </form>
-      </tr>
-    </tfoot>
-  </table>
-  <button type="button" class="btn btn-outline-dark" type="button" onclick="location.href='addapointment.php'">　　予定を追加する　　</button>
+<body class="team">
+
+
+<form action="?" method="post">  
+
+   
+<body class="team">
+</div>
+
+
+</div>
+    <div class="container">
+        <h3><div class = mb-5><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</div></a></h3>
+        <table class="table table-bordered">
+            <tr>
+                <th>日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th>土</th>
+            </tr>
+            
+            <?php
+                foreach ($weeks as $week) {
+                    echo $week;
+                }
+            ?>
+            </h1>
+
+        </table>
+        
+
+        <div class="sakusei">
+        <button type = "submit" formaction="../apointment/Apointment_check.php" class="buto">全予定を 確認する</button>
+        </div>
+        <div class="sakuseia">
+        <button type = "submit" formaction="../apointment/AddApointment.php" class="buto">予定を 追加する</button>
+        </div>
+        
+
+    </div>
+
+
+    </form>
 </body>
 </html>
